1227460bba32d5db717c49b616a7de5aefaeb64b
edwin-huang <104748091+edwin-huang@users.noreply.github.com>
2023-10-25 18:37:45 -0700

    Update rushb.m

--- rushb.m
+++ rushb.m
@@ -1,8 +1,32 @@
 %brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5')
+%autonomously operates the robot vehicle
+%does not generalize to all mazes (which i assume will be milestone 2)
 display('P90 gogogo'); 
-meme = 90;
-while (1)
+
+%while (1)
+    move(brick, 1);
     turn(brick, 1);
+    move(brick, 1);
+    turn(brick, 1);
+    move(brick, 1);
+    move(brick, 1);
+    brick.MoveMotor('C', 100);
+    pause(5);
+    %brick.MoveMotor('C', -100);
+    %pause(5);
+    brick.MoveMotor('C', 0);
+    turn(brick, -1);
+    turn(brick, -1);
+    move(brick, 1);
+    move(brick, 1);
+    turn(brick, -1);
+    move(brick, 1);
+    move(brick, 1);
+    turn(brick, -1);
+    move(brick, 1);
+    move(brick, 1);
+    turn(brick, -1);
+    move(brick, 1);
     %move(brick, 1);
     %move(brick, -1);
-end
+%end

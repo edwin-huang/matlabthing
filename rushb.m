00391bcfbc403fb5589747bf35322e4e0f33d43c
edwin-huang <104748091+edwin-huang@users.noreply.github.com>
2023-11-08 22:06:33 -0700

    Update rushb.m

--- rushb.m
+++ rushb.m
@@ -1,32 +1,5 @@
 %brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5')
 %autonomously operates the robot vehicle
-%does not generalize to all mazes (which i assume will be milestone 2)
-display('P90 gogogo'); 
-
-%while (1)
-    move(brick, 1);
-    turn(brick, 1);
-    move(brick, 1);
-    turn(brick, 1);
-    move(brick, 1);
-    move(brick, 1);
-    brick.MoveMotor('C', 100);
-    pause(5);
-    %brick.MoveMotor('C', -100);
-    %pause(5);
-    brick.MoveMotor('C', 0);
-    turn(brick, -1);
-    turn(brick, -1);
-    move(brick, 1);
-    move(brick, 1);
-    turn(brick, -1);
-    move(brick, 1);
-    move(brick, 1);
-    turn(brick, -1);
-    move(brick, 1);
-    move(brick, 1);
-    turn(brick, -1);
-    move(brick, 1);
-    %move(brick, 1);
-    %move(brick, -1);
-%end
+%can travel the maze by hugging the left wall but cannot pick up the
+%passenger nor stop
+wrapper(brick);

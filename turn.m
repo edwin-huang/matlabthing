1d0df426e9605b133bf401787e03b752189654d3
edwin-huang <104748091+edwin-huang@users.noreply.github.com>
2023-11-08 22:07:12 -0700

    Delete turn.m

--- turn.m
+++
@@ -1,8 +0,0 @@
-function turn(brick, sign)
-    brick.MoveMotor('A', -20 * sign);
-    brick.MoveMotor('B', 20 * sign);
-    pause(1.4);
-    brick.MoveMotor('A', 0);
-    brick.MoveMotor('B', 0);
-    pause(1.6);
-end

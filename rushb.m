b090efd492a54b0f222934c3b018fb63dfe82d7f
edwin-huang <104748091+edwin-huang@users.noreply.github.com>
2023-11-22 18:43:03 -0700

    Update rushb.m

--- rushb.m
+++ rushb.m
@@ -1,5 +1,3 @@
 %brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5')
 %autonomously operates the robot vehicle
-%can travel the maze by hugging the left wall but cannot pick up the
-%passenger nor stop
 wrapper(brick);
